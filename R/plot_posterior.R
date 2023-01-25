d76b4437b94ddf863430431b505797c24e935cc7
jovoni <santacatterinagiovanni@gmail.com>
2023-01-25 17:17:20 +0100

    Small changes posterior plots

--- R/plot_posterior.R
+++ R/plot_posterior.R
@@ -1,100 +1,62 @@
 
 #' Plot birth and death rates posteriors
 #'
-#' @param x a bipod object with a 'fit' field
-#' @param point_est Character string indicating which point estimate to use. Available options are "mean", "median", and "none". Default is "mean".
+#' @param x a bipod object with a 'fit' field.
 #'
 #' @return A ggplot object containing the posterior density plots of the birth and death rates and the prior density plot
 #' @export
 #'
-plot_birth_and_death_rates_posteriors = function(x, point_est = c("mean", "median", "none")) {
+plot_birth_and_death_rates_posteriors = function(x) {
   # Check input
   if (!(inherits(x, "bipod"))) stop("Input must be a bipod object")
   if (!("fits" %in% names(x))) stop("Input must contain a 'fits' field")
 
-  # Check point_est and select corresponding function
-  point_est <- match.arg(point_est)
-  if (point_est == "median") {
-    m_func = stats::median
-  } else if (point_est == "mean") {
-    m_func = base::mean
-  } else {
-    m_func = mode.fun
-  }
+  # # Check point_est and select corresponding function
+  # point_est <- match.arg(point_est)
+  # if (point_est == "median") {
+  #   m_func = stats::median
+  # } else if (point_est == "mean") {
+  #   m_func = base::mean
+  # } else {
+  #   m_func = mode.fun
+  # }
 
   # Obtain list of parameters to plot
   par_list = c("lambda", "mu")
 
-  # Prepare prior data
-  if (x$fit_info$prior == "uniform") {
-    xs <- seq(x$fit_info$a - 0.1, x$fit_info$b + 0.1, length=1000)
-    prior_data <- data.frame(
-      x = xs,
-      y = dunif(xs, x$fit_info$a, x$fit_info$b)
-    ) %>%
-      na.omit()
-  } else if (x$fit_info$prior == "invgamma") {
-    # plot prior, 90 % of the prior if possible, or at max twice the max of the
-    x_lim <- invgamma::qinvgamma(.9, x$fit_info$a, x$fit_info$b)
-
-    xs <- seq(0.001, x_lim, length=1000)
-    prior_data <- data.frame(
-      x = xs,
-      y = invgamma::dinvgamma(xs, x$fit_info$a, x$fit_info$b)
-    ) %>%
-      na.omit()
-  } else {
-    cli::cli_alert_danger("The prior {.var x$fit_info$prior} has not been recognized")
-  }
-
   # Plot for every fit
   plots <- lapply(names(x$fits), function(fit_name) {
     fit <- x$fits[[fit_name]]
+
+    # Prepare prior data
+    prior_data <- prepare_prior_data(x, par = "lambda")
+
     # Prepare data
     d_long <- rstan::extract(fit, pars = par_list) %>%
       as.data.frame() %>%
       dplyr::rename_at(par_list, ~paste0(par_list, gsub("fit", "", fit_name))) %>%
       reshape2::melt(id.vars=NULL)
 
-    d <- d_long %>%
-      dplyr::group_by(.data$variable) %>%
-      dplyr::summarise(
-        point_est = point_est,
-        # l = quantile(.data$value, probs[1]),
-        m  = m_func(.data$value),
-        # h = quantile(.data$value, probs[2])
-      )
+    if (x$fit_info$prior == "invgamma") {
+      xlims <- c(0 - 0.1, max(d_long$value) + 0.1)
+    } else {
+      xlims <- c(x$fit_info$a - 0.1, x$fit_info$b + 0.1)
+    }
+
+    # Filter data
+    prior_data <- prior_data %>%
+      filter(.data$x >= xlims[1] & .data$x <= xlims[2])
+
+    d_long <- d_long %>%
+      filter(.data$value >= xlims[1] & .data$value <= xlims[2])
 
     # plot posterior density
     p <- ggplot2::ggplot(d_long, ggplot2::aes(x=.data$value)) +
-      ggplot2::geom_histogram(ggplot2::aes(y = ggplot2::after_stat(density)), binwidth = 0.01, alpha = .3) +
+      ggplot2::geom_histogram(ggplot2::aes(y = ggplot2::after_stat(density)), binwidth = .01, alpha = .3) +
       ggplot2::geom_density(col = "forestgreen", size = .8) +
       ggplot2::facet_wrap( ~ .data$variable)
 
-    # Plot estimates, if requested
-    if (point_est != "none") {
-      p <- p +
-        ggplot2::geom_vline(
-          data = d,
-          ggplot2::aes(xintercept = .data$m),
-          col = "forestgreen",
-          linetype = 'longdash',
-          size = .5,
-          show.legend = FALSE
-        )
-      # ggplot2::annotate(
-      #   geom = 'rect',
-      #   xmin = d$l,
-      #   xmax = d$h,
-      #   ymin = 0,
-      #   ymax = Inf,
-      #   color = NA,
-      #   fill = 'forestgreen',
-      #   alpha = .4
-      # ) +
-      # ggplot2::facet_wrap( ~ variable)
-    }
-
+    # Plot prior
     p <- p +
       ggplot2::geom_line(
         data = prior_data,
@@ -109,6 +71,7 @@ plot_birth_and_death_rates_posteriors = function(x, point_est = c("mean", "media
         y = 'density',
         x = "value"
       ) +
+      coord_cartesian(xlim=xlims) +
       my_ggplot_theme()
   })
 
@@ -119,61 +82,20 @@ plot_birth_and_death_rates_posteriors = function(x, point_est = c("mean", "media
 #' Plot growth rates posteriors
 #'
 #' @param x a bipod object with a 'fit' field
-#' @param point_est Character string indicating which point estimate to use. Available options are mean", "median", and "none". Default is "mean".
 #'
 #' @return A ggplot object containing the posterior density plots of the growth rates and the prior density plot
 #' @export
 #'
-plot_growth_rate_posteriors = function(x, point_est = c("mean", "median", "none")) {
+plot_growth_rate_posteriors = function(x) {
   # Check input
   if (!(inherits(x, "bipod"))) stop("Input must be a bipod object")
   if (!("fits" %in% names(x))) stop("Input must contain a 'fits' field")
-
-  # Check point_est and select corresponding function
-  point_est <- match.arg(point_est)
-  if (point_est == "median") {
-    m_func = median
-  } else if (point_est == "mean") {
-    m_func = mean
-  } else {
-    m_func = mode.fun
-  }
 
   # Obtain list of parameters to plot
   par_list = c("ro")
 
   # Prepare prior data
-  limits <- lapply(x$fits, function(fit) {
-    d_long <- rstan::extract(fit, pars = par_list) %>%
-      as.data.frame() %>%
-      reshape2::melt(id.vars=NULL)
-
-    x_max <- max(d_long$value)
-    x_min <- min(d_long$value)
-    return(c(x_min, x_max))
-  })
-  x_min <- min(unlist(limits))
-  x_max <- max(unlist(limits))
-
-  if (x$fit_info$prior == "uniform") {
-    xs = seq(x_min, x_max, length=1000)
-    prior_data <- data.frame(
-      x = xs,
-      y = pdf_triangular(xs, x$fit_info$a, x$fit_info$b)
-    ) %>%
-      na.omit() %>%
-      dplyr::filter(dplyr::between(x, x_min, x_max))
-
-  } else if (x$fit_info$prior == "invgamma") {
-    lambda_prior <- invgamma::rinvgamma(10000, x$fit_info$a, x$fit_info$b)
-    mu_prior <- invgamma::rinvgamma(10000, x$fit_info$a, x$fit_info$b)
-    prior_data <- data.frame(x = lambda_prior - mu_prior) %>%
-      na.omit() %>%
-      dplyr::filter(dplyr::between(x, x_min + .1, x_max - .1))
-
-  } else {
-    cli::cli_alert_danger("The prior {.var x$fit_info$prior} has not been recognized")
-  }
+  prior_data <- prepare_prior_data(x, par = "ro")
 
   plots <- lapply(names(x$fits), function(fit_name) {
     fit <- x$fits[[fit_name]]
@@ -183,51 +105,24 @@ plot_growth_rate_posteriors = function(x, point_est = c("mean", "median", "none"
       dplyr::rename_at(par_list, ~paste0(par_list, gsub("fit", "", fit_name))) %>%
       reshape2::melt(id.vars=NULL)
 
-    d <- d_long %>%
-      dplyr::group_by(.data$variable) %>%
-      dplyr::summarise(
-        point_est = point_est,
-        # l = quantile(.data$value, probs[1]),
-        m  = m_func(.data$value),
-        # h = quantile(.data$value, probs[2])
-      )
+    # plot posterior density
+    xlims <- c(min(d_long$value) * .99, max(d_long$value) * 1.01)
+    bw = (xlims[2] - xlims[1]) / 100
+
+    # Filter data
+    prior_data <- prior_data %>%
+      filter(.data$x >= xlims[1] & .data$x <= xlims[2])
+
+    d_long <- d_long %>%
+      filter(.data$value >= xlims[1] & .data$value <= xlims[2])
 
     # plot posterior density
-    x_max <- max(d_long$value)
-    x_min <- min(d_long$value)
-    bw = (x_max - x_min) / 100
-
     p <- ggplot2::ggplot(d_long, ggplot2::aes(x=.data$value)) +
-      ggplot2::geom_histogram(aes(y = after_stat(density)), binwidth = bw, alpha = .3) +
+      ggplot2::geom_histogram(ggplot2::aes(y = ggplot2::after_stat(density)), binwidth = bw, alpha = .3) +
       ggplot2::geom_density(col = "forestgreen", size = .8) +
       ggplot2::facet_wrap( ~ .data$variable)
 
-    # Plot estimates, if requested
-    if (point_est != "none") {
-      p <- p +
-        ggplot2::geom_vline(
-          data = d,
-          ggplot2::aes(xintercept = .data$m),
-          col = "forestgreen",
-          linetype = 'longdash',
-          size = .5,
-          show.legend = FALSE
-        ) +
-        # ggplot2::annotate(
-        #   geom = 'rect',
-        #   xmin = d$l,
-        #   xmax = d$h,
-        #   ymin = 0,
-        #   ymax = Inf,
-        #   color = NA,
-        #   fill = 'forestgreen',
-        #   alpha = .4
-        # ) +
-        ggplot2::facet_wrap( ~ .data$variable)
-    }
-
     # Create prior of ro sampling from the two priors over lambda and mu
-
     if (x$fit_info$prior == "uniform") {
       p <- p +
         ggplot2::geom_line(
@@ -253,7 +148,7 @@ plot_growth_rate_posteriors = function(x, point_est = c("mean", "median", "none"
         y = 'density',
         x = "value"
       ) +
-      ggplot2::coord_cartesian(xlim=c(x_min - bw, x_max + bw)) +
+      ggplot2::coord_cartesian(xlim=xlims) +
       my_ggplot_theme()
   })
 
@@ -273,5 +168,66 @@ pdf_triangular <- function(z, a, b) {
   prob[(z >= a) & (z <= (a+b)/2)] <- 2*(z[(z >= a) & (z <= (a+b)/2)] - a)/((b-a)^2)
   prob[(z > (a+b)/2) & (z <= b)] <- 2*(b - z[(z > (a+b)/2) & (z <= b)])/((b-a)^2)
   return(prob)
+}
+
+prepare_prior_data = function(x, par) {
+  if (par %in% c("ro")) {
+    limits <- lapply(x$fits, function(fit) {
+      d_long <- rstan::extract(fit, pars = par_list) %>%
+        as.data.frame() %>%
+        reshape2::melt(id.vars=NULL)
+
+      x_max <- max(d_long$value)
+      x_min <- min(d_long$value)
+      return(c(x_min, x_max))
+    })
+    x_min <- min(unlist(limits))
+    x_max <- max(unlist(limits))
+
+    if (x$fit_info$prior == "uniform") {
+      xs = seq(x_min, x_max, length=1000)
+      prior_data <- data.frame(
+        x = xs,
+        y = pdf_triangular(xs, x$fit_info$a, x$fit_info$b)
+      ) %>%
+        na.omit() %>%
+        dplyr::filter(dplyr::between(x, x_min, x_max))
+
+    } else if (x$fit_info$prior == "invgamma") {
+      lambda_prior <- invgamma::rinvgamma(10000, x$fit_info$a, x$fit_info$b)
+      mu_prior <- invgamma::rinvgamma(10000, x$fit_info$a, x$fit_info$b)
+      prior_data <- data.frame(x = lambda_prior - mu_prior) %>%
+        na.omit() %>%
+        dplyr::filter(dplyr::between(x, x_min + .1, x_max - .1))
+
+    } else {
+      cli::cli_alert_danger("The prior {.var x$fit_info$prior} has not been recognized")
+    }
+
+  } else if (par %in% c("lambda", "mu")) {
+    if (x$fit_info$prior == "uniform") {
+      xs <- seq(x$fit_info$a - 0.1, x$fit_info$b + 0.1, length=1000)
+      prior_data <- data.frame(
+        x = xs,
+        y = dunif(xs, x$fit_info$a, x$fit_info$b)
+      ) %>%
+        na.omit()
+    } else if (x$fit_info$prior == "invgamma") {
+      # plot prior, 90 % of the prior if possible, or at max twice the max of the
+      x_lim <- invgamma::qinvgamma(.9, x$fit_info$a, x$fit_info$b)
+
+      xs <- seq(0.001, x_lim, length=1000)
+      prior_data <- data.frame(
+        x = xs,
+        y = invgamma::dinvgamma(xs, x$fit_info$a, x$fit_info$b)
+      ) %>%
+        na.omit()
+    } else {
+      cli::cli_alert_danger("The prior {.var x$fit_info$prior} has not been recognized")
+    }
+  } else {
+    stop("par not recognized")
+  }
+  return(prior_data)
 }
 
