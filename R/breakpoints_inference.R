3f422edc5327c539e6fe86bcc32e41c0d6a354fe
jovoni <giovy.santa@gmail.com>
2023-06-07 17:26:14 +0200

    Added tuning of lambda

--- R/breakpoints_inference.R
+++ R/breakpoints_inference.R
@@ -4,7 +4,7 @@
 #' @param dt half-size of the interval to consider for each proposed breakpoints
 #' @param growth_type character string specifying the type of growth assumed,
 #'  one of "exponential", "logistic".
-#' @param spline_spar degree of smoothness of the spline to use
+#' @param lambda degree of smoothness of the spline to use
 #' @param variational Boolean specifying whether using variational as opposed to mcmc sampling
 #' @param factor_size numeric factor by which to divide counts in the bipod object
 #' @param prior_K Prior mean for the carrying capacity.
@@ -12,13 +12,14 @@
 #' @param chains integer number of chains to run in the Markov Chain Monte Carlo (MCMC) algorithm
 #' @param iter integer number of iterations to run in the MCMC algorithm
 #' @param cores integer number of cores to use in parallel processing
+#' @param desired_changepoints .
 #'
 #' @return the input bipod object with an added 'breakpoints_fit' slot containing the fitted model for the breakpoints
 #' @export
 breakpoints_inference <- function(x, dt = NULL,
                                   growth_type = "exponential",
                                   variational = FALSE,
-                                  spline_spar = .5,
+                                  lambda = .5, desired_changepoints = NULL,
                                   factor_size = 1, prior_K = NULL,
                                   model_selection = FALSE,
                                   chains = 4, iter = 4000, cores = 4) {
@@ -29,7 +30,12 @@ breakpoints_inference <- function(x, dt = NULL,
   sampling_type <- if (variational) "variational inference" else "MCMC sampling"
 
   # Prepare input data
-  input_data <- biPOD:::prep_data_bp_inference(x = x, factor_size = factor_size, prior_K = prior_K, dt = dt, spline_spar = spline_spar)
+  input_data <- biPOD:::prep_data_bp_inference(x = x, factor_size = factor_size, prior_K = prior_K, dt = dt, lambda = lambda, n_nodes = desired_changepoints)
+
+  if (is.null(input_data)) {
+    cli::cli_alert_danger("Not possible to infer the desired number of break points!")
+    return(x)
+  }
 
   if (length(input_data$changing_times_prior) == 0) {
     cli::cli_alert_danger("Not possible to infer break points. Zero probable breakpoints were found!")
@@ -58,7 +64,6 @@ breakpoints_inference <- function(x, dt = NULL,
     elbo_d <- res$elbo_d
   } else {
     sampling <- "mcmc"
-    fit_model <- model$sample(data = input_data, chains = chains, parallel_chains = cores, iter_warmup = iter, iter_sampling = iter, refresh = iter)
     tmp <- utils::capture.output(suppressMessages(fit_model <- model$sample(data = input_data, chains = chains, parallel_chains = cores, iter_warmup = iter, iter_sampling = iter, refresh = iter)))
   }
 
@@ -78,7 +83,7 @@ breakpoints_inference <- function(x, dt = NULL,
 
   # Add median of breakpoints
   n_changepoints <- length(input_data$changing_times_prior)
-  breakpoints_names <- lapply(n_changepoints, function(i) {
+  breakpoints_names <- lapply(1:n_changepoints, function(i) {
     paste0("changing_times[", i, "]")
   }) %>% unlist()
 
--- R/utils_breakpoints_inference.R
+++ R/utils_breakpoints_inference.R
@@ -1,4 +1,4 @@
-prep_data_bp_inference <- function(x, factor_size = 1, prior_K = NULL, dt = NULL, spline_spar = .5) {
+prep_data_bp_inference <- function(x, factor_size = 1, prior_K = NULL, dt = NULL, lambda = .5, n_nodes = NULL) {
   # Parameters check
   if (is.null(prior_K)) {
     prior_K <- max(x$counts$count) / factor_size
@@ -8,25 +8,18 @@ prep_data_bp_inference <- function(x, factor_size = 1, prior_K = NULL, dt = NULL
   }
 
   # Get spline prior for changing times
-  spl <- stats::smooth.spline(x = x$counts$time, y = x$counts$count, spar = spline_spar)
+  xs <- x$counts$time
+  ys <- x$counts$count
 
-  x_smooth <- seq(min(x$counts$time), max(x$counts$time), length = 1000)
-  y_smooth <- stats::predict(spl, x = x_smooth)[2] %>% unlist()
-  dy_smooth <- stats::predict(spl, x = x_smooth, deriv = 1)[2] %>% unlist()
-
-  dy <- diff(y_smooth) / diff(x_smooth)
-  dy <- c(dy[1], dy)
-
-  sign_dy <- sign(dy)
-  change_idx <- lapply(2:length(dy), function(i) {
-    if (sign_dy[i - 1] != sign_dy[i]) {
-      return(i)
-    }
-  }) %>%
-    unlist() %>%
-    stats::na.omit()
-
-  change_x <- x_smooth[change_idx]
+  if (!(is.null(n_nodes))) {
+    tuned_lambda <- tune_lambda(xs, ys, n_nodes = n_nodes)
+    if (is.null(tuned_lambda)) {
+      return(NULL)
+    } # not found
+    change_x <- spline_nodes(xs, ys, tuned_lambda)
+  } else {
+    change_x <- spline_nodes(xs, ys, lambda)
+  }
 
   if (is.null(dt)) {
     dt <- min(diff(change_x)) / 2
@@ -45,4 +38,57 @@ prep_data_bp_inference <- function(x, factor_size = 1, prior_K = NULL, dt = NULL
   )
 
   return(input_data)
+}
+
+
+tune_lambda <- function(xs, ys, n_nodes) {
+  upper <- 1
+  lower <- 0
+
+  n_lower <- n_spline_nodes(xs, ys, lower)
+  n_upper <- n_spline_nodes(xs, ys, upper)
+
+  if ((n_lower > n_nodes) & (n_upper > n_nodes)) {
+    return(NULL)
+  }
+  if ((n_lower < n_nodes) & (n_upper < n_nodes)) {
+    return(NULL)
+  }
+
+  while (TRUE) {
+    mid <- (lower + upper) / 2 # Calculate the midpoint of the interval
+    if (n_spline_nodes(xs, ys, mid) == n_nodes) {
+      return(mid)
+    } else if (n_spline_nodes(xs, ys, mid) > n_nodes) {
+      lower <- mid # Update the lower bound if f(mid) < n
+    } else {
+      upper <- mid # Update the upper bound otherwise
+    }
+  }
+}
+
+spline_nodes <- function(xs, ys, lambda, n_points = 1000) {
+  spl <- stats::smooth.spline(x = xs, y = ys, spar = lambda)
+
+  x_smooth <- seq(min(xs), max(xs), length = n_points)
+  y_smooth <- stats::predict(spl, x = x_smooth)[2] %>% unlist()
+  dy_smooth <- stats::predict(spl, x = x_smooth, deriv = 1)[2] %>% unlist()
+
+  dy <- diff(y_smooth) / diff(x_smooth)
+  dy <- c(dy[1], dy)
+
+  sign_dy <- sign(dy)
+  change_idx <- lapply(2:length(dy), function(i) {
+    if (sign_dy[i - 1] != sign_dy[i]) {
+      return(i)
+    }
+  }) %>%
+    unlist() %>%
+    stats::na.omit()
+
+  x_smooth[change_idx]
+}
+
+n_spline_nodes <- function(xs, ys, lambda, n_points = 1000) {
+  length(spline_nodes(xs, ys, lambda, n_points))
 }
